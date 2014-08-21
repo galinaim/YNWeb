a8b1e7ba3447429259ae3268ccfc176fee699ba0
galinaim <galinaim@walla.com>
2014-08-21 13:31:08 +0300

    New page for glass volumetric equipment
    new images for volumetric instruments and a new PHP page to view them.

---
+++ volglass.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html lang="he" >
+   <head>
+     <title>ציוד מדידה זכוכית</title>
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <meta charset="UTF-8">
+     
+     <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="css/styles.css" rel="stylesheet" media="screen">
+    <link href="css/mystyle.css" rel="stylesheet" media="screen">
+
+    <!--[if !IE 7]>
+    <style type="text/css">
+    #wrap {display:table;height:100%}
+    </style>
+    <![endif]-->
+     
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="assets/js/html5shiv.js"></script>
+    <script src="assets/js/respond.min.js"></script>
+    <![endif]-->
+  
+     
+   </head>
+   
+   <body>
+       
+       <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+       <script src="http://code.jquery.com/jquery.min.js">
+       </script>
+       <!-- Include all compiled plugins (below), or include individual files as needed -->
+       <script src="js/bootstrap.min.js">
+	</script>
+        
+    
+        <div class="wrap">
+            
+            <div class="mycontainer">
+                <nav class="navbar navbar-default navbar-fixed-top navbar-inverse" role="navigation">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                    </div>
+                    
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse navbar-ex1-collapse ">
+                        <ul class="nav navbar-nav navbar-right  "  id="menu_items">
+                            <li class="active navbar-right"><a href="3.html">דף הבית</a></li>
+                            <li class="navbar-right"><a href="#">מי אנחנו?</a></li>
+                            <li class="navbar-right"> <a href="#">נעים בל-ארט</a></li>
+                            <li class="navbar-right"> <a href="#">נציגויות</a></li>
+                            <li class="navbar-right"> <a href="#">מפת הגעה</a></li>
+                            <li class="navbar-right"><a href="#">צור קשר</a></li>
+                        </ul>
+                        
+                        <form class="navbar-form navbar-left" role="search" action="Try.php" method="get">
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="search">
+                            </div>
+                            <button type="submit" class="btn btn-default">חיפוש</button>
+			</form>
+                        
+                    </div><!-- /.navbar-collapse -->
+                        
+                </nav>
+                
+            </div>
+        
+           
+            <div class="main">
+                <!--jumbotron-->
+                <div class="jumbotron">
+                    <div class="container" style="direction: rtl">
+                        <h2 class='products1'>כלי מדידה מזכוכית CLASS A</h2>
+                        <p>ציוד המדידה של חברת BRAND הנרכש אצל חברת יואל נעים בע"מ, שהיא המשווק הרשמי והמורשה הבלעדי של BRAND בישראל, מספק למשתמש דיוק מקסימלי עם כאב ראש מינימלי!</p>
+                        <p>כל כלי המדידה מזכוכית Class A הינם תוצרת גרמניה ומיוצרים תחת בדיקות קפדניות וביקורות איכות הן במהלך הייצור והן בסופו. הכלים מאושרים לשימוש במעבדות GLP  ו-ISO 17025. 
+                        על כל כלי מדידה מוטבע מספר אצווה, ובכל אריזה רשמית של ברנד מצורפת תעודת כיול לאצווה כולה.
+                        מוצרי BRAND בליווי תעודות הכיול המצורפות מקובלים על הרשויות המסמיכות ומבטיחים ביקורות קצרות ומוצלחות.
+                        </p>
+                        <p>תעודות הכיול של BRAND מצורפות אך ורק לאריזות סגורות. לא ניתן לקבל חלקי אריזה.</p>
+                        <p>כל כלי המדידה מכוילים לפי תקני ה-ISO, אך ניתן להזמין גם כלי מדידה המכוילים לפי תקני ה-USP או ה-DKD, ואליהם מצורפות תעודות הכיול המתאימות.</p>
+                        <p><a class="btn btn-primary btn-lg" role="button" href="http://www.brand.de/en/products/volumetric-instruments/blaubrandr-quality/" target="_blank">לפרטים נוספים</a>
+                            <a class="btn btn-primary btn-lg" role="button" href="http://catalog.brand.de/index.php?chapter=2&xtree=1&begin=0&ID_O_TREE_GROUP=2&sLanguage=English&start_infoblock=1" target="_blank">לקטלוג האינטרנטי</a>
+                        <a class="btn btn-primary btn-lg" role="button" href="#">לקבלת הצעת מחיר</a></p>
+                    </div>            
+                </div>
+                
+                            <!-- Vertical nav-bar -->
+            <div style="height: 100%;">
+                <div id='cssmenu'>
+                    <ul>
+                        <li class='active has-sub'><a href='#'><span>כלי מדידה</span></a>
+                            <ul>
+                                <li class='has-sub'><a href='volglass.php'><span>זכוכית</span></a></li>
+                                <li class='has-sub'><a href='#'><span>פלסטיק</span></a></li>
+                            </ul>
+                        </li>
+                        <li><a href='#'><span>מוצרים מתכלים</span></a></li>
+                        <li><a href='#'><span>מכשור לעבודה עם נוזלים</span></a></li>
+                        <li><a href='#'><span>תמיסות</span></a></li>
+                        <li><a href='#'><span>עבודות בית מלאכה</span></a></li>
+                        <li><a href='#'><span>ניקיון כלים</span></a></li>
+                        <li><a href='#'><span>מעבדת כיולים</span></a></li>
+                        <li><a href='#'><span>ניירות סינון</span></a></li>
+                        <li><a href='#'><span>ערכות לבדיקת מים</span></a></li>
+                        <li><a href='#'><span>כרומוטוגרפיה</span></a></li>
+                        <li><a href='#'><span>טרמומטרים והידרומטרים</span></a></li>
+                        <li><a href='#'><span>סטנדרטים</span></a></li>
+                        <li><a href='#'><span>ציוד מאושר למזון</span></a></li>
+                        <li><a href='#'><span>הדרכות ויעוץ</span></a></li>
+                        <li class='last'><a href='#'><span>סרטוני הדרכה</span></a></li>
+                    </ul>
+                </div>
+            </div>
+                
+                <!--content-->
+                <div class="content">
+                <div class="container marketing" align="middle" style="width: auto; height: auto;">
+                <div class="row" align="middle">
+                        <div style="width: 1000px; height:640px;">
+                            <div class="col-sm-6 col-md-4" style="height: auto; width: auto; float: right;">
+                                <div class="col-xs-6 col-md-3" style="height: auto; width: auto;">
+                                    <div class="thumbnail">
+                                        <img src="images/glass volumetric/flask.jpg" alt="בקבוק מדידה" class="prod" style="height:200px; width: 120px;">
+                                        <div class="caption" style="height: auto; width: 220px;">
+                                            <h3>בקבוק מדידה</h3>
+                                            <p>בקבוקי מדידה CLASS A תוצרת BRAND גרמניה.</p>
+                                            <p>מכוילים לפי תקן DIN EN ISO 1042 ומלווים בתעודות כיול לאצווה.</p>
+                                            <p>נפחים 1-500 מ"ל: ארוז בזוגות</p>
+                                            <p>נפחים 1-10 ליטר: ארוז בודד</p>
+                                            <p><a href="http://www.brand.de/en/products/volumetric-instruments/volumetric-flasks/volumetric-flasks-glass/" target="_blank" class="btn btn-primary" role="button">לפרטים</a></p>
+                                            <p><a href="http://catalog.brand.de/index.php?encrypt=0&ID_O_TREE_GROUP=249&chapter=249&ID_O_PRODUCT=128&begin=1&sLanguage=English&start_infoblock=1" target="_blank" class="btn btn-primary" role="button">לקטלוג: נפחים 1-2 מ"ל</a></p>
+                                            <p><a href="http://catalog.brand.de/index.php?encrypt=0&ID_O_TREE_GROUP=249&chapter=249&ID_O_PRODUCT=134&begin=1&sLanguage=English&start_infoblock=1" target="_blank" class="btn btn-primary" role="button">לקטלוג: נפחים 5 מ"ל-10 ליטר</a></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+             
+                            <div class="col-sm-6 col-md-4" style="height: auto; width: auto; float: right;">
+                                <div class="col-xs-6 col-md-3" style="height: auto; width: auto;">
+                                    <div class="thumbnail">
+                                        <img src="images/glass volumetric/flaskamber.jpg" alt="בקבוק מדידה חום" class="prod" style="height:200px; width: 120px;">
+                                        <div class="caption" style="height: auto; width: 220px;">
+                                            <h3>בקבוק מדידה חום</h3>
+                                            <p>בקבוקי מדידה חומים CLASS A תוצרת BRAND גרמניה.</p>
+                                            <p>מכוילים לפי תקן DIN EN ISO 1042 ומלווים בתעודות כיול לאצווה.</p>
+                                            <p>נפחים 5-500 מ"ל: ארוז בזוגות</p>
+                                            <p>1 ליטר: ארוז בודד</p>
+                                            <p><a href="http://www.brand.de/en/products/volumetric-instruments/volumetric-flasks/volumetric-flasks-glass/" target="_blank" class="btn btn-primary" role="button">לפרטים</a>
+                                            <a href="http://catalog.brand.de/index.php?encrypt=0&ID_O_TREE_GROUP=249&chapter=249&ID_O_PRODUCT=136&begin=1&sLanguage=English&start_infoblock=1" target="_blank" class="btn btn-primary" role="button">לקטלוג</a></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+             
+                            <div class="col-sm-6 col-md-4" style="height: auto; width: auto; float: right;">
+                                <div class="col-xs-6 col-md-3" style="height: auto; width: auto;">
+                                    <div class="thumbnail">
+                                        <img src="images/glass volumetric/bulbpipette.jpg" alt="פיפטה מילוי" class="prod" style="height:200px; width: 100px;">
+                                            <div class="caption" style="height: auto; width: 220px;">
+                                                <h3>פיפטה מילוי</h3>
+                                                <p>פיפטות מילוי CLASS AS תוצרת BRAND גרמניה.</p>
+                                                <p>מכוילות לפי תקן DIN EN ISO 648 ומלוות בתעודות כיול לאצווה.</p>
+                                                <p>נפחים 0.5-2 מ"ל: אריזות של 12 יח'</p>
+                                                <p>נפחים 2.5-100 מ"ל: אריזות של 6 יח'</p>
+                                                <p><a href="http://www.brand.de/en/products/volumetric-instruments/bulb-pipettes/" target="_blank" class="btn btn-primary" role="button">לפרטים</a>
+                                                    <a href="http://catalog.brand.de/index.php?encrypt=0&ID_O_TREE_GROUP=228&chapter=228&ID_O_PRODUCT=106&begin=1&sLanguage=English&start_infoblock=1" target="_blank" class="btn btn-primary" role="button">לקטלוג</a></p>
+                                            </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    <br>
+                        <div style="width: auto; height: auto;">             
+                            <div class="col-sm-6 col-md-4" style="height: auto; width: auto; float: right;">
+                                <div class="col-xs-6 col-md-3" style="height: auto; width: auto;">
+                                    <div class="thumbnail">
+                                        <img src="images/glass volumetric/gradpipette.jpg" alt="פיפטה מדידה" class="prod" style="height:200px; width: 100px;">
+                                        <div class="caption" style="height: auto; width: 220px;">
+                                            <h3>פיפטה מדידה</h3>
+                                            <p>פיפטות מדידה CLASS AS תוצרת BRAND גרמניה.</p>
+                                            <p>מכוילות לפי תקן DIN EN ISO 835 ומלוות בתעודות כיול לאצווה.</p>
+                                            <p>נפחים 0.5-10 מ"ל: אריזות של 12 יח'</p>
+                                            <p>נפחים 20-50 מ"ל: אריזות של 6 יח'</p>
+                                            <p><a href="http://www.brand.de/en/products/volumetric-instruments/graduated-pipettes/" target="_blank" class="btn btn-primary" role="button">לפרטים</a>
+                                                <a href="http://catalog.brand.de/index.php?encrypt=0&ID_O_TREE_GROUP=222&chapter=222&ID_O_PRODUCT=91&begin=1&sLanguage=English&start_infoblock=1" target="_blank" class="btn btn-primary" role="button">לקטלוג</a></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+     
+                            <div class="col-sm-6 col-md-4" style="height: auto; width: auto; float: right;">
+                                <div class="col-xs-6 col-md-3" style="height: auto; width: auto;">
+                                    <div class="thumbnail">
+                                    <img src="images/glass volumetric/cylinder.jpg" alt="משורה" class="prod" style="height:200px; width: 100px;">
+                                        <div class="caption" style="height: auto; width: 220px;">
+                                            <h3>משורה</h3>
+                                            <p>משורות CLASS A תוצרת BRAND גרמניה.</p>
+                                            <p>מכוילות לפי תקן DIN EN ISO 4788 ומלוות בתעודות כיול לאצווה.</p>
+                                            <p>נפחים 5-500 מ"ל: ארוזות בזוגות</p>
+                                            <p>נפחים 1-2 ליטר: ארוז בודד</p>
+                                            <p><a href="http://www.brand.de/en/products/volumetric-instruments/graduatedmixing-cylinders/graduated-cylinders-mixing-cylinders-glass/" target="_blank" class="btn btn-primary" role="button">לפרטים</a>
+                                            <a href="http://catalog.brand.de/index.php?encrypt=0&ID_O_TREE_GROUP=243&chapter=243&ID_O_PRODUCT=116&begin=1&sLanguage=English&start_infoblock=1" target="_blank" class="btn btn-primary" role="button">לקטלוג</a></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+             
+                            <div class="col-sm-6 col-md-4" style="height: auto; width: auto; float: right;">
+                                <div class="col-xs-6 col-md-3" style="height: auto; width: auto;">
+                                    <div class="thumbnail">
+                                        <img src="images/glass volumetric/burette.jpg" alt="בירטה" class="prod" style="height:200px; width: 100px;">
+                                        <div class="caption" style="height: auto; width: 220px;">
+                                            <h3>בירטה</h3>
+                                            <p>בירטה ישרה ברז טפלון CLASS A תוצרת BRAND גרמניה.</p>
+                                            <p>מכוילת לפי תקן DIN EN ISO 385 ומלווה בתעודת כיול לאצווה.</p>
+                                            <p>נפחים 10, 25 ו-50 מ"ל</p>
+                                            <p>ארוזות בזוגות</p>
+                                            <p><a href="http://www.brand.de/en/products/volumetric-instruments/burettes-automatic-burettes/" target="_blank" class="btn btn-primary" role="button">לפרטים</a>
+                                            <a href="http://catalog.brand.de/index.php?encrypt=0&ID_O_TREE_GROUP=207&chapter=207&ID_O_PRODUCT=13&begin=1&sLanguage=English&start_infoblock=1" target="_blank" class="btn btn-primary" role="button">לקטלוג</a></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+             
+                        </div>
+                    </div>
+                </div>
+                </div>
+        </div>
+         
+        <!--footer-->
+        <div id="footer">
+            <p class="pull-right"><a href="#">Back to top</a></p>
+            <p class="pull-left" style="direction:ltr">© Gali Translate Corporation</p>
+        </div>
+        
+                
+  
+    </body>
+    
+</html>
