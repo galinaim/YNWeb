fe426e6a934b2ad4e8d3c2f07a6ed769bf64506f
galinaim <galinaim@walla.com>
2014-08-24 12:44:17 +0300

    Plastic volumetric instruments page

---
+++ volplastic.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html lang="he" >
+   <head>
+     <title>ציוד מדידה פלסטיק</title>
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <meta charset="UTF-8">
+     
+     <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="css/styles.css" rel="stylesheet" media="screen">
+    <link href="css/mystyle.css" rel="stylesheet" media="screen">
+
+    <!--[if !IE 7]>
+    <style type="text/css">
+    #wrap {display:table;height:100%}
+    </style>
+    <![endif]-->
+     
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="assets/js/html5shiv.js"></script>
+    <script src="assets/js/respond.min.js"></script>
+    <![endif]-->
+  
+     
+   </head>
+   
+   <body>
+       
+       <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+       <script src="http://code.jquery.com/jquery.min.js">
+       </script>
+       <!-- Include all compiled plugins (below), or include individual files as needed -->
+       <script src="js/bootstrap.min.js">
+	</script>
+        
+    
+        <div class="wrap">
+            
+            <div class="mycontainer">
+                <nav class="navbar navbar-default navbar-fixed-top navbar-inverse" role="navigation">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                    </div>
+                    
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse navbar-ex1-collapse ">
+                        <ul class="nav navbar-nav navbar-right  "  id="menu_items">
+                            <li class="active navbar-right"><a href="3.html">דף הבית</a></li>
+                            <li class="navbar-right"><a href="#">מי אנחנו?</a></li>
+                            <li class="navbar-right"> <a href="#">נעים בל-ארט</a></li>
+                            <li class="navbar-right"> <a href="#">נציגויות</a></li>
+                            <li class="navbar-right"> <a href="#">מפת הגעה</a></li>
+                            <li class="navbar-right"><a href="#">צור קשר</a></li>
+                        </ul>
+                        
+                        <form class="navbar-form navbar-left" role="search" action="Try.php" method="get">
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="search">
+                            </div>
+                            <button type="submit" class="btn btn-default">חיפוש</button>
+			</form>
+                        
+                    </div><!-- /.navbar-collapse -->
+                        
+                </nav>
+                
+            </div>
+        
+           
+            <div class="main">
+                <!--jumbotron-->
+                <div class="jumbotron">
+                    <div class="container" style="direction: rtl">
+                        <h2 class='products1'>כלי מדידה מפלסטיק </h2>
+                        <p>הדיוק נשאר אבל לא נשבר!</p>
+                        <p>חברת יואל נעים בע"מ מציעה מגוון כלי מדידה מפלסטיק תוצרת BRAND גרמניה:</p>
+                        <p>כלי מדידה PP: לעבודה נוחה, פשוטה ועמידה מפני שברים.</p>
+                        <p>כלי מדידה PMP: כלי מדידה CLASS A עם תעודות כיול המאושרים לשימוש במעבדות GLP  ו-ISO 17025</p>
+                        <p>כלי מדידה PFA: כלי מדידה CLASS A עם תעודות כיול המאושרים לשימוש במעבדות GLP  ו-ISO 17025 ומותאמים לעבודה עם חומצות ובסיסים שפוגעים בזכוכית.</p>
+                            <a class="btn btn-primary btn-lg" role="button" href="http://catalog.brand.de/index.php?chapter=2&xtree=1&begin=0&ID_O_TREE_GROUP=2&sLanguage=English&start_infoblock=1" target="_blank">לקטלוג האינטרנטי</a>
+                        <a class="btn btn-primary btn-lg" role="button" href="#">לקבלת הצעת מחיר</a></p>
+                    </div>            
+                </div>
+                
+                            <!-- Vertical nav-bar -->
+            <div style="height: 100%;">
+                <div id='cssmenu'>
+                    <ul>
+                        <li class='active has-sub'><a href="#"><span>כלי מדידה</span></a>
+                            <ul>
+                                <li class='has-sub'><a href='volglass.php'><span>זכוכית</span></a></li>
+                                <li class='has-sub'><a href='volplastic.php'><span>פלסטיק</span></a></li>
+                            </ul>
+                        </li>
+                        <li><a href='#'><span>מוצרים מתכלים</span></a></li>
+                        <li><a href='#'><span>מכשור לעבודה עם נוזלים</span></a></li>
+                        <li><a href='#'><span>תמיסות</span></a></li>
+                        <li><a href='#'><span>עבודות בית מלאכה</span></a></li>
+                        <li><a href='#'><span>ניקיון כלים</span></a></li>
+                        <li><a href='#'><span>מעבדת כיולים</span></a></li>
+                        <li><a href='#'><span>ניירות סינון</span></a></li>
+                        <li><a href='#'><span>ערכות לבדיקת מים</span></a></li>
+                        <li><a href='#'><span>כרומוטוגרפיה</span></a></li>
+                        <li><a href='#'><span>טרמומטרים והידרומטרים</span></a></li>
+                        <li><a href='#'><span>סטנדרטים</span></a></li>
+                        <li><a href='#'><span>ציוד מאושר למזון</span></a></li>
+                        <li><a href='#'><span>הדרכות ויעוץ</span></a></li>
+                        <li class='last'><a href='#'><span>סרטוני הדרכה</span></a></li>
+                    </ul>
+                </div>
+            </div>
+                
+                <!--content-->
+                <div class="content">
+                <div class="container marketing" align="middle" style="width: auto; height: auto;">
+                <div class="row" align="middle">
+                        <div style="width: 1000px; height:640px;">
+                            <div class="col-sm-6 col-md-4" style="height: auto; width: auto; float: right;">
+                                <div class="col-xs-6 col-md-3" style="height: auto; width: auto;">
+                                    <div class="thumbnail">
+                                        <img src="images/plastic volumetric/PMP flask.jpg" alt="בקבוק מדידה" class="prod" style="height:200px; width: 130px; display: inline;">
+                                        <img src="images/plastic volumetric/PFA flasks.jpg" alt="בקבוק מדידה" class="prod" style="height:200px; width: 225px; display: inline;">
+                                        <div class="caption" style="height: 280px; width: 415px;">
+                                            <h3>בקבוק מדידה</h3>
+                                            <p>בקבוקי מדידה מפלסטיק CLASS A ו-CLASS B תוצרת BRAND גרמניה.</p>
+                                            <p>בקבוקי מדידה PFA CLASS A עם פקק הברגה ותעודת כיול, מכויל לפי תקן DIN EN ISO 1042. עמיד באוטוקלאב. נפחים 10-500 מ"ל.</p>
+                                            <p>בקבוקי מדידה  PMP CLASS A עם פקק לטש ותעודת כיול, מכויל לפי תקן DIN EN ISO 1042. עמיד באוטוקלאב. נפחים 10-1000 מ"ל.</p>
+                                            <p>בקבוקי מדידה PP CLASS B עם פקק לטש. נפחים 10-1000 מ"ל.</p>
+                                            <p>לקטלוג: 
+                                            <a href="http://catalog.brand.de/index.php?encrypt=0&ID_O_TREE_GROUP=395&chapter=395&ID_O_PRODUCT=127&begin=1&sLanguage=English&start_infoblock=1" target="_blank" class="btn btn-primary" role="button">PFA</a>
+                                            <a href="http://catalog.brand.de/index.php?encrypt=0&ID_O_TREE_GROUP=395&chapter=395&ID_O_PRODUCT=26292&begin=1&sLanguage=English&start_infoblock=1" target="_blank" class="btn btn-primary" role="button">PMP</a>
+                                            <a href="http://catalog.brand.de/index.php?encrypt=0&ID_O_TREE_GROUP=395&chapter=395&ID_O_PRODUCT=125&begin=1&sLanguage=English&start_infoblock=1" target="_blank" class="btn btn-primary" role="button">PP</a></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+             
+                            <div class="col-sm-6 col-md-4" style="height: auto; width: auto; float: right;">
+                                <div class="col-xs-6 col-md-3" style="height: auto; width: auto;">
+                                    <div class="thumbnail">
+                                        <img src="images/plastic volumetric/PMP cylinder.jpg" alt="משורה" class="prod" style="height:200px; width: 120px; display: inline;">
+                                        <img src="images/plastic volumetric/PP cylinder.jpg" alt="משורה" class="prod" style="height:200px; width: 120px; display: inline;">
+                                        <div class="caption" style="height: 280px; width: 355px;">
+                                            <h3>משורה</h3>
+                                            <p>משורות מפלסטיק CLASS A ו-CLASS B תוצרת BRAND גרמניה.</p>
+                                            <p>משורות  PMP CLASS A כיתוב כחול עם תעודת כיול לאצווה. מכוילות לפי תקן DIN 12681 ISO 6706. עמידות באוטוקלאב. נפחים 10-2000 מ"ל.</p>
+                                            <p>משורות PP CLASS B כיתוב כחול או כיתוב שקוף מובלט. נפחים 10-2000 מ"ל.</p>
+                                            <p>לקטלוג: 
+                                            <a href="http://catalog.brand.de/index.php?encrypt=0&ID_O_TREE_GROUP=328&chapter=328&ID_O_PRODUCT=124&begin=1&sLanguage=English&start_infoblock=1" target="_blank" class="btn btn-primary" role="button">PMP</a>
+                                            <a href="http://catalog.brand.de/index.php?encrypt=0&ID_O_TREE_GROUP=328&chapter=328&ID_O_PRODUCT=123&begin=1&sLanguage=English&start_infoblock=1" target="_blank" class="btn btn-primary" role="button">PP</a></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+             
+        
+                            <div class="col-sm-6 col-md-4" style="height: auto; width: auto; float: right;">
+                                <div class="col-xs-6 col-md-3" style="height: auto; width: auto;">
+                                    <div class="thumbnail">
+                                        <img src="images/plastic volumetric/PP bulb.jpg" alt="פיפטה מילוי" class="prod" style="height:200px; width: 100px;">
+                                        <div class="caption" style="height: auto; width: 250px;">
+                                            <h3>פיפטה מילוי</h3>
+                                            <p>פיפטות מילוי PP תוצרת BRAND גרמניה.</p>
+                                            <p>נפחים 1-2 מ"ל: אריזות של 12 יח'</p>
+                                            <p>נפחים 5-50 מ"ל: אריזות של 6 יח'</p>
+                                            <p><a href="http://catalog.brand.de/index.php?encrypt=0&ID_O_TREE_GROUP=324&chapter=324&ID_O_PRODUCT=108&begin=1&sLanguage=English&start_infoblock=1" target="_blank" class="btn btn-primary" role="button">לקטלוג</a></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+     
+                            <div class="col-sm-6 col-md-4" style="height: auto; width: auto; float: right;">
+                                <div class="col-xs-6 col-md-3" style="height: auto; width: auto;">
+                                    <div class="thumbnail">
+                                        <img src="images/plastic volumetric/PP grad.jpg" alt="משורה" class="prod" style="height:200px; width: 100px;">
+                                        <div class="caption" style="height: auto; width: 250px;">
+                                            <h3>פיפטה מדידה</h3>
+                                            <p>פיפטות מדידה PP תוצרת BRAND גרמניה.</p>
+                                            <p>נפחים 1-10 מ"ל: אריזות של 12 יח'</p>
+                                            <p><a href="http://catalog.brand.de/index.php?encrypt=0&ID_O_TREE_GROUP=325&chapter=325&ID_O_PRODUCT=88&begin=1&sLanguage=English&start_infoblock=1" target="_blank" class="btn btn-primary" role="button">לקטלוג</a></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </div>
+        </div>
+         
+        <!--footer-->
+        <div id="footer">
+            <p class="pull-right"><a href="#">Back to top</a></p>
+            <p class="pull-left" style="direction:ltr">© Gali Translate Corporation</p>
+        </div>
+        
+                
+  
+    </body>
+    
+</html>
